ab0eb98e26db4d40136a6bd56b2dc634d0d5dc13
borisdvlpr <eduffmiranda@gmail.com>
2022-01-03 04:31:06 +0000

    WIP - Added menu button.

--- public/app.js
+++ public/app.js
@@ -1,8 +1,7 @@
 var i = 0;
 let i2 = 0;
-var txt = "\u205f-\u205fDepartamento\u205fInformática!"; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
-
+var txt = "\u205f-\u205fDepartamento\u205fInformática"; /* The text */
+var speed = 100;                                       /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
@@ -26,6 +25,32 @@ function delTypeWriter() {
     }
 }
 
+function menuButton() {
+    let menuButton = document.querySelector('.menu-button');
+    var st = window.getComputedStyle(menuButton, null);
+    var tm = st.getPropertyValue('transform') || "none";
 
-setInterval(typeWriter, 3000);
-setInterval(delTypeWriter, 8000);
+    menuButton.addEventListener('click', function() {
+        if (tm != 'none') {
+            let values = tm.split('(')[1].split(')')[0].split(',');
+            let angle = Math.round(Math.atan2(values[1],values[0]) * (180/Math.PI));
+
+            if(angle > 0) {
+                angle = angle - 180;
+                menuButton.style.transform = 'rotate('+angle+'deg)';
+
+            } else { 
+                angle = angle + 180;
+                menuButton.style.transform = 'rotate('+angle+'deg)';
+            };
+        }
+    }); 
+}
+
+function main() {
+    menuButton();
+    setInterval(typeWriter, 3000);
+    setInterval(delTypeWriter, 8000);
+}
+
+main()
