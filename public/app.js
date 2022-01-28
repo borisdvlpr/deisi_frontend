5781cd5a903153759d764d409f7294366105396c
borisdvlpr <eduffmiranda@gmail.com>
2022-01-28 08:07:44 +0000

    WIP - menu button change.

--- public/app.js
+++ public/app.js
@@ -17,22 +17,17 @@ function detailsManager() {
 function toggleMenu() {
     let menuButton = document.querySelector('.menu-button');
     let menuDrawer = document.querySelector('.menu-drawer');
-    var st = window.getComputedStyle(menuButton, null);
-    var tm = st.getPropertyValue('transform') || "none";
+    const burger = document.querySelector(".burger");
 
-    menuButton.addEventListener('click', function() {
-        if (tm != 'none') {
-            if(angle > 0) {
-                angle = angle - 180;
-                menuButton.style.transform = 'rotate('+angle+'deg)';
-                menuDrawer.style.transform = 'translateX(0%)';
+    menuButton.addEventListener("click", () => {
+        if(menuDrawer.style.transform === 'translateX(0%)') {
+            menuDrawer.style.transform = 'translateX(-120%)';
 
-            } else { 
-                angle = angle + 180;
-                menuButton.style.transform = 'rotate('+angle+'deg)';
-                menuDrawer.style.transform = 'translateX(-120%)';
-            };
+        } else {
+            menuDrawer.style.transform = 'translateX(0%)';
         }
+        
+        burger.classList.toggle("toggle");
     }); 
 }
 
